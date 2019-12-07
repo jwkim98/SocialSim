a054529c0ba2f8bb92ebbaab98c772f63c053654
ilbeom <lemon1495@gmail.com>
2019-12-07 18:26:45 +0900

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,24 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import copy
 
-def read_file():
-    data_person = pd.read_csv("data_person.csv",names=["PersonID","Selflessness","Selfishness","Money","Strength","Power"])
-    data_relation = pd.read_csv("data_relation.csv", names = ["Subjet", "Target", "relation", "probability"])
+def read_file(n):
+    data_person = pd.read_csv("People_{}.csv".format(n),names=["GroupID", "PersonID","Selflessness","Selfishness","Money","Strength","Power"])
+    data_relation = pd.read_csv("Relationship_{}.csv".format(n), names = ["Subjet", "Target", "relation", "probability"])
     return data_person, data_relation
 
-def read_file2():
-    file1 = open("data_person.csv",'r')
-    file2 = open("data_relation.csv",'r')
-    data_person = file1.read().split()
-    data_relation = file2.read().split()
-    for i in range(len(data_person)):
-        data_person[i] = data_person[i].split(",")
-    for i in range(len(data_relation)):
-        data_relation[i] = data_relation[i].split(",")
-    data_person = pd.DataFrame(np.array(data_person),columns = ["PersonID","Selflessness","Selfishness","Money","Strength","Power"])
-    data_relation = pd.DataFrame(np.array(data_relation), columns = ["Subjet", "Target", "relation", "probability"])
-    return data_person, data_relation
+def read_entire_data(N):
+    d1, d2 = read_file("input")
+    people = [d1]
+    relationship = [d2]
+    for i in range(1, N+1):
+        d1, d2 = read_file(i)
+        people.append(d1)
+        relationship.append(d2)
+    return people, relationship
+
+def grouping(data):
+    average = dict()
+    for i in range(len(data.index)):
+        group = data.iloc[i,0]
+        if group not in average.keys():
+            average[group] = [0,0,0,0]
+        average[group][0] += 1
+        if data["Money"][i] != 0:
+            average[group][1] += 1
+        average[group][2] += int(data["Money"][i])
+        average[group][3] += int(data["Power"][i])
+    for i in average:
+        average[i][2] = average[i][2]/average[i][0]
+        average[i][3] = average[i][3]/average[i][0]
+    return pd.DataFrame(average,index = ["Total", "Alive", "Money", "Power"]).transpose()
+
+def time_based(data_set, name):
+    collection = {}
+    for i in data_set[0].index:
+        collection[i] = []
+        for j in data_set:
+            collection[i].append(j[name][i])
+    return pd.DataFrame(collection)
+
+
+
 
 def relation_average(data_relation):
     average = dict()
@@ -32,14 +57,34 @@ def relation_average(data_relation):
 def power_relation(data_person, relation_average):
     power = data_person[["Power"]]
     data = pd.concat([power,relation_average],axis=1)
-    return power
+    return data
+
+def count_alive(data_person):
+    data = data_person.loc[data_person["Money"]!=0]
+    return len(data["Money"])
+
     
 def main():
-    d1, d2 = read_file()
-    print(d1)
-    print(d2)
-    d3 = relation_average(d2)
-    print(d3)
-    d4 = power_relation(d1, d3)
-    print(d4)
+    N = 1
+    people, relationship = read_entire_data(N)
+    people_group = []
+    for i in people:
+        people_group.append(grouping(i))
+    time_based_alive = time_based(people_group,"Alive")
+    time_based_money = time_based(people_group,"Money")
+    time_based_power = time_based(people_group,"Power")
+    plt.figure(1, figsize = (9,12))
+    plt.subplot(311)
+    plt.plot(time_based_alive)
+    plt.ylabel("Alive")    
+    plt.subplot(312)
+    plt.plot(time_based_money)
+    plt.ylabel("Money")    
+    plt.subplot(313)
+    plt.plot(time_based_power)
+    plt.xlabel("Time")
+    plt.ylabel("Power")
+    plt.figure(2)
+    
+    
 main()
