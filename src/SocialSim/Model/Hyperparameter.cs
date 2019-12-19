621b1443f9b9927652177a37d4292f7bf5eb6b81
Unknown <jwkim98@kaist.ac.kr>
2019-12-19 18:47:02 +0900

    refactor : adjust parameters

--- src/SocialSim/Model/Hyperparameter.cs
+++ src/SocialSim/Model/Hyperparameter.cs
@@ -9,7 +9,7 @@ namespace SocialSim.Model
         /// <summary>
         ///  Degree of random value being multiplied
         /// </summary>
-        public static readonly double RandomDegree = 1;
+        public static readonly double RandomDegree = 0.5;
 
         public static readonly double RelationDegree = 1;
 
@@ -21,12 +21,12 @@ namespace SocialSim.Model
         public static readonly int MinimumStealBetweenGoodEvil = 150;
         public static readonly int MaximumStealBetweenGoodEvil = 250;
 
-        public static readonly double MinimumRelationshipDecreaseBetweenEvil = 0.2;
-        public static readonly double MinimumRelationshipDecreaseGoodEvil = 0.3;
+        public static readonly double MinimumRelationshipDecreaseBetweenEvil = 0.01;
+        public static readonly double MinimumRelationshipDecreaseGoodEvil = 0.015;
 
-        public static readonly double RelationshipIncreaseBetweenGood = 1.1;
+        public static readonly double RelationshipIncreaseBetweenGood = 1.5;
 
-        public static readonly double StanceThresholdBetweenGoodEvil = 0.5;
+        public static readonly double StanceThresholdBetweenGoodEvil = 0.0;
 
         public static readonly double RelationshipRatioToFrequency = 0.3;
 
